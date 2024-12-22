ff9f3497497e48a66876a92e5e7c087086332c80
Lavique <lavique.oliveira@maisunifacisa.com.br>
2024-12-22 15:41:08 -0300

    criação da entidade Todo

---
+++ src/main/java/br/com/laviquedias/api_todo_list/entities/Todo.java
@@ -0,0 +1,41 @@
+package br.com.laviquedias.api_todo_list.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id_todo")
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_todo;
+
+    @Column(nullable = false)
+    private String nome_todo;
+    
+    @Column(nullable = false)
+    private String descricao_todo;
+    
+    @Column(nullable = false)
+    private boolean realizadoo_todo;
+    
+    @Column(nullable = false)
+    private int prioridade_todo;
+
+}
