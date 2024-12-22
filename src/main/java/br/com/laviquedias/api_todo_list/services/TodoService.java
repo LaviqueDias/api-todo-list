1338e3e9c72459788c869b87d4a46ae2d25c5305
Lavique <lavique.oliveira@maisunifacisa.com.br>
2024-12-22 16:04:10 -0300

    criação do CRUD no TodoService

---
+++ src/main/java/br/com/laviquedias/api_todo_list/services/TodoService.java
@@ -0,0 +1,59 @@
+package br.com.laviquedias.api_todo_list.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import br.com.laviquedias.api_todo_list.entities.Todo;
+import br.com.laviquedias.api_todo_list.repositories.TodoRepository;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    
+
+    public Set<Todo> create(Todo todo){
+        todoRepository.save(todo);
+
+        return list();
+    }
+
+
+    public Set<Todo> list(){
+        Sort sort = Sort.by("prioridade_todo").descending().and(
+            Sort.by("nome").ascending()
+        );
+
+        Set<Todo> todos = new HashSet<>();
+
+        for (Todo todo : todoRepository.findAll(sort)) {
+            todos.add(todo);
+        }
+
+        return todos;
+
+    }
+
+
+    public Set<Todo> update(Todo todo){
+        todoRepository.save(todo);
+
+        return list();
+    }
+
+
+    public Set<Todo> delete(Long id_todo){
+        todoRepository.deleteById(id_todo);
+
+        return list();
+    }
+
+}
